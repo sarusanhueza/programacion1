cc8e3c3f76361f3753429bf57dba1cf396597e02
sarusanhueza <sarusanhueza2@gmail.com>
2022-05-13 17:17:23 -0300

    no tengo idea

---
+++ fetch_funcion_asincrono/main.js
@@ -0,0 +1,9 @@
+async function obtener_datos()
+{
+    // fetch realiza una solicitud http a trevez del metodo GET
+    const datos = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const datos_definitivos = await datos.json()
+    datos_definitivos.forEach(element => {
+        console.log (element.title)
+    });
+}
