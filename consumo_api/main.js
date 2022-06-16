352b32d8d6fad0f2928daece455350b8b48b7d48
sarusanhueza <sarusanhueza2@gmail.com>
2022-06-16 17:13:50 -0300

    nada

--- consumo_api/main.js
+++ consumo_api/main.js
@@ -2,19 +2,19 @@ const consumir_datos = async () =>{
         const datos_recibidos = await fetch('https://apis.datos.gob.ar/georef/api/provincias')
     
         const lista = await datos_recibidos.json()
-        
-        function generar(){
-            let fila = []
-                datos_recibidos.forEach((element,index) => {
+         let fila = []
+                lista.provincias.forEach((element,index) => {
                   let filita = 
                  ` <tr>
                       <td>${index+1}</td>
-                      <td>${element}</td>
+                      <td>${element.nombre}</td>
                   </tr>`
                   fila.push(filita)
                     
                 });
                 document.getElementById("tablita").innerHTML = fila.join('')
+        
             
-            }
         }
+
+        consumir_datos()
