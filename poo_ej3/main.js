e4caf00f19b24582d2d3983ff491f5e9b9a273c3
sarusanhueza <sarusanhueza2@gmail.com>
2022-06-30 17:02:56 -0300

    poo localStorage

--- poo_ej3/main.js
+++ poo_ej3/main.js
@@ -1,1 +1,18 @@
 import Producto from "./producto.js";
+
+function guardar(){
+    let Descripcion = document.getElementById("Descripcion").value
+    let precio_venta = document.getElementById("Precio").value
+    let categoria = document.getElementById("select").value
+
+    let producto = new Producto(Descripcion,precio_venta,categoria)
+    producto.guardar_producto()
+
+}
+document.getElementById("bon").addEventListener("click", guardar)
+
+function listar(){
+    let producto = new Producto()
+    producto.obtener_productos()
+}
+listar()
--- poo_ej3/producto.js
+++ poo_ej3/producto.js
@@ -2,12 +2,34 @@ export default class Producto {
 
 
 
-    constructor(){
+    constructor(d,p,c){
+        this.Descripcion = d
+        this.precio_venta = p
+        this.categoria = c
+
 
     }
 
 
     guardar_producto(){
-        
+
+        let new_product = {
+            descripcion : this.Descripcion,
+            Precio : this.precio_venta,
+            Categoria : this.categoria
+        }
+
+        if("produccto" in localStorage){
+            let lista_productos = JSON.parse(localStorage.getItem("produccto"))
+            lista_productos.push(new_product)
+            localStorage.setItem("produccto",JSON.stringify(lista_productos))
+        } 
+
+        else 
+        {let lista = []
+        lista.push(new_product)
+        localStorage.setItem("produccto", JSON.stringify(lista))}
+
+
     }
 }
