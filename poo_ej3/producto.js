c00bc2f16cbd7eda3c52fa5120f79b26dd3cc91d
sarusanhueza <sarusanhueza2@gmail.com>
2022-07-01 17:12:57 -0300

    cambio de poo

--- poo_ej3/producto.js
+++ poo_ej3/producto.js
@@ -6,15 +6,12 @@ export default class Producto {
         this.Descripcion = d
         this.precio_venta = p
         this.categoria = c
-
-
     }
-
 
     guardar_producto(){
 
         let new_product = {
-            descripcion : this.Descripcion,
+            Descripcion : this.Descripcion,
             Precio : this.precio_venta,
             Categoria : this.categoria
         }
@@ -29,7 +26,27 @@ export default class Producto {
         {let lista = []
         lista.push(new_product)
         localStorage.setItem("produccto", JSON.stringify(lista))}
+    }
 
+    /*recolectamos todos los productos alojados en el item "producctos" que estan en notacion json. 
+    Debemos convertirlo en notacion nativa de javaScrip ((es decir, hacemos JSON.parce()))
+    */
+ 
+    obtener_productos(){
+        let listaa = JSON.parse(localStorage.getItem("produccto"))
+        let filas = []
+        listaa.forEach((element,index) => {
+            let fila = 
+            `<tr>
+                 <td>${index+1}</td>
+                 <td>${element.Descripcion}</td>
+                 <td>${element.Precio}</td>
+                 <td>${element.Categoria}</td>
 
+            </tr>`
+            filas.push(fila)
+            
+        });
+        document.getElementById("tb").innerHTML = filas.join('')
     }
 }
